e900d5456439a5365746e985fe70c577b80a99c4
“yotalha” <talha.coder9@gmail.com>
2023-03-06 01:42:29 +0500

    Implementing components and props

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Giga Chad</h1>
+      <Job salary={90000} position="Senior SDE" company="Amazon" />
+      <Job salary={12000} position="Junior SDE" company="Google" />
+      <Job salary={10000} position="Project Manager" company="Netflix" />
     </div>
   );
+}
+
+function Job(props) {
+  return (
+    <div>
+      <h1>Salary: {props.salary}</h1>
+      <h2>Position: {props.position}</h2>
+      <h2>Company: {props.company}</h2>
+    </div>
+  )
 }
 
 export default App;
